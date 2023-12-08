ffdc3aab57fcc425b29c37c211fa30a3cc60456f
root <root@localhost>
2023-12-08 23:53:07 +0800

    auto publish 1702050786627

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Add all your JS here
+
+function yesFunc() {
+  document.getElementById("message").innerHTML = "ihh, enebe hehe";
+}
+
+function noFunc() {
+  document.getElementById("message").innerHTML = "ah hindi? sge sge, aus aus";
+}
